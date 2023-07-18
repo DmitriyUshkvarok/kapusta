cceb690ab92129bb694bf7d077401ff2e9dd637d
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-07-18 16:16:26 +0300

    add mobile styles

--- src/components/ButtonBackMobile/ButtonBackMobile.jsx
+++ src/components/ButtonBackMobile/ButtonBackMobile.jsx
@@ -1,14 +1,21 @@
 import Link from 'next/link';
+import styles from '@/src/sass/components/_buttonBackMobile.module.scss';
 
 export default function ButtonBackMobile({ pathname }) {
   return (
     <Link
+      className={styles.linkStyled}
       href={{
         pathname: '/transaction',
         query: { name: pathname },
       }}
     >
-      to transaction
+      <span>
+        <svg width={18} height={18}>
+          <use xlinkHref="/sprite.svg#icon-keyboard_backspace-24px-1" />
+        </svg>
+      </span>
+      <span>to transaction</span>
     </Link>
   );
 }
--- src/components/NavBalance/NavBalance.jsx
+++ src/components/NavBalance/NavBalance.jsx
@@ -1,14 +1,29 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import styles from '@/src/sass/components/_navBalance.module.scss';
 
 export default function NavBalance() {
+  const pathname = usePathname();
+
   return (
-    <ul>
-      <li>
-        <Link href="/balance/expense">expense</Link>
-      </li>
-      <li>
-        <Link href="/balance/income">income</Link>
-      </li>
-    </ul>
+    <div className={styles.container}>
+      <ul className={styles.listNav}>
+        <li
+          className={`${styles.itemNav} ${
+            pathname === '/balance/expense' ? styles.active : ''
+          }`}
+        >
+          <Link href="/balance/expense">expense</Link>
+        </li>
+        <li
+          className={`${styles.itemNav} ${
+            pathname === '/balance/income' ? styles.active : ''
+          }`}
+        >
+          <Link href="/balance/income">income</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
