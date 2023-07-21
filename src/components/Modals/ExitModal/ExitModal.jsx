9baf632f43d46e6f7ac37802b5ead712530709fc
OPaslavskiy <bankir.op@gmail.com>
2023-07-21 13:48:02 +0300

    styled for exitModal and СonfirmationExitModal

--- src/components/Modals/ExitModal/ExitModal.jsx
+++ src/components/Modals/ExitModal/ExitModal.jsx
@@ -1,5 +1,19 @@
+import styles from '@/src/sass/components/_exitModal.module.scss';
+import { AiOutlineClose } from 'react-icons/ai';
+
 const ExitModal = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <AiOutlineClose className={styles.closeBtn} />
+      <div className={styles.textModal}>Are you sure?</div>
+      <button type="button" className={`${styles.btn} ${styles.btnAccept}`}>
+        Yes
+      </button>
+      <button type="button" className={`${styles.btn} ${styles.btnReject}`}>
+        No
+      </button>
+    </div>
+  );
 };
 
 export default ExitModal;
